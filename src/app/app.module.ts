fceefcab3243012e566d50948383fdf2a1dd449a
Silas <silascaimi@live.com>
2020-09-03 12:12:58 -0300

    create communication with child using @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from "@angular/core";
 
 import { EventsAppComponent } from "./events-app.component";
 import { EventsListComponent } from "./events/events-list.component";
+import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 
 @NgModule({
-  declarations: [EventsAppComponent, EventsListComponent],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+  ],
   imports: [BrowserModule],
   bootstrap: [EventsAppComponent],
 })
