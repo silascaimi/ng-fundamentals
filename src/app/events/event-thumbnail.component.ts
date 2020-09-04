326f15f21103fcebc5c0b3bae83e7014b796822f
Silas <silascaimi@live.com>
2020-09-03 21:17:18 -0300

    create template variables to interect with child

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -18,4 +18,9 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 })
 export class EventThumbnailComponent {
   @Input() event: any;
+  someProperty: any = "some value";
+
+  logFoo() {
+    console.log("foo");
+  }
 }
