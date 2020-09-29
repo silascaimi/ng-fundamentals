df345abc237cd1f7511b335af021b1654068593a
Silas <sila.caimi@capgemini.com>
2020-09-28 21:30:22 -0300

    style component with ngClass method one

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,13 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     <div class="well hoverwell thumbnail">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">
+      <div
+        [ngClass]="{
+          green: event?.time === '8:00 am',
+          bold: event?.time === '8:00 am'
+        }"
+        [ngSwitch]="event?.time"
+      >
         Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -26,6 +32,9 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
     `
       .green {
         color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
       }
       .thumbnail {
         min-height: 210px;
